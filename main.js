f8c2c3b6f0d7784a60f1e294cd657c6d5f8015c1
Test Student <99680427+iacs-test-student@users.noreply.github.com>
2022-09-27 12:57:33 +0000

    ++typescript

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+import './test'
+document.querySelector('#app').innerHTML = `
+  <h1>Test Publishing Vite Project</h1>
+  Wow oh wow I'm going to publish this on the internet!
+`
---
+++ test.ts
@@ -0,0 +1,3 @@
+let h2 = document.createElement('h2');
+h2.textContent = 'WOW here is some content I put in with typescript';
+document.querySelector('body').appendChild(h2);
